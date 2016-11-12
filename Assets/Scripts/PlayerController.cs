303228aa2962b642c1e390d32caaf5845d6b16d5
Jerry Chen <jchen0022@gmail.com>
2016-11-12 14:57:45 -0800

    basic movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    // Update is before rendering
+    // FixedUpdate is right before any physics calculations
+    
+    public float speed;
+    private Rigidbody2D rigidBody;
+
+    void Start() {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+    
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVeritcal = Input.GetAxisRaw("Vertical");
+        
+        Vector2 movement = new Vector2(moveHorizontal, moveVeritcal);
+
+        rigidBody.AddForce(movement * speed);
+    }
+
+}
