aaff1de7c3bbe86af16066e183b77fdfc1e3bcc5
Jerry Chen <jchen0022@gmail.com>
2016-11-12 18:39:26 -0800

    DropManager.cs

--- Assets/Scripts/DropManager.cs
+++ Assets/Scripts/DropManager.cs
@@ -5,17 +5,22 @@ public class DropManager : MonoBehaviour {
 
 	// Use this for initialization
     public GameObject player;
+    public GameObject drop;
+    private bool isDropping;
 
-	void Start () {
-        player = GameObject.Find("Player 1");
+    void Start () {
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+    // Update is called once per frame
+    void Update () {
+        isDropping = Input.GetKey("space");
+        if (isDropping) {
+            Spawn();
+        }
 	}
 
     void Spawn() {
         Vector3 spawnPoint = player.transform.position;
+        Instantiate(drop, spawnPoint, Quaternion.identity);
     }
 }
