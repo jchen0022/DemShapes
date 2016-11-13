f95f19716378faa9ce087fa583e7977773e0e5bf
Jerry Chen <jchen0022@gmail.com>
2016-11-12 17:03:46 -0800

    player drop and assets

---
+++ Assets/Scripts/DropManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropManager : MonoBehaviour {
+
+	// Use this for initialization
+    public GameObject player;
+
+	void Start () {
+        player = GameObject.Find("Player 1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Spawn() {
+        Vector3 spawnPoint = player.transform.position;
+    }
+}
---
+++ Assets/Scripts/PlayerAttributes.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttributes : MonoBehaviour {
+
+    private MaterialPropertyBlock propBlock;
+
+	// Use this for initialization
+	void Start () {
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        renderer.color = new Color (0f, 0f, 0f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
